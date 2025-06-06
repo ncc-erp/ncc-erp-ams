e786d9251ef6767013c8f416bd61474f30de271a
VinhPhamNCC <vinh.phamquang@ncc.asia>
2025-06-06 17:18:46 +0700

    Add maintenance for comsumables (#45)

--- resources/lang/en/admin/consumables/general.php
+++ resources/lang/en/admin/consumables/general.php
@@ -1,11 +1,13 @@
 <?php
 
 return array(
-    'checkout'                          => 'Checkout Consumable to User',
-    'consumable_name'                   => 'Consumable Name',
-    'create'                            => 'Create Consumable',
-    'item_no'                           => 'Item No.',
-    'remaining' 			            => 'Remaining',
-    'total' 			                => 'Total',
-    'update'                            => 'Update Consumable',
+    'checkout' => 'Checkout Consumable to User',
+    'consumable_name' => 'Consumable Name',
+    'create' => 'Create Consumable',
+    'item_no' => 'Item No.',
+    'remaining' => 'Remaining',
+    'total' => 'Total',
+    'update' => 'Update Consumable',
+    'months' => 'months',
+
 );
