647263eaf22143a59bc690517fa83e468ee6a6bb
Cao Nam <caonam030402@gmail.com>
2025-06-10 17:57:41 +0700

    fix: remove check authorization

--- app/Http/Requests/HRMUserHookRequest.php
+++ app/Http/Requests/HRMUserHookRequest.php
@@ -5,17 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 
 class HRMUserHookRequest extends FormRequest
-{
-     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-    
+{   
     public function rules()
     {
         return [
